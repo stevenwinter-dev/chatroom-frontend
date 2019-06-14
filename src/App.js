6b6fd528db5301da977853f254a961c4842f0b1d
Steven Winter <stevenwinter.me@gmail.com>
2019-06-13 20:49:08 -0500

    Completed upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Login from './Login';
+import ChatContainer from './ChatContainer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends Component {
+  state = {
+    username: null
+  }
+
+  setUsername = (username) => {
+    this.setState({username});
+  } 
+  render(){
+    return (
+      <div className="App">
+        {
+          !this.state.username ?
+            <Login setUsername={this.setUsername} />
+            :
+            <ChatContainer username={this.state.username} /> 
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
